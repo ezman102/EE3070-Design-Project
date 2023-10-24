05d7b83432319560b54817b888407c87a7a34c5d
SIN Yuk Yuen <yysin8-c@my.cityu.edu.hk>
2023-10-24 11:10:05 +0800

    ok

--- script.js
+++
@@ -1,7 +0,0 @@
-// Add event listeners for keypad buttons
-document.getElementById('btn1').addEventListener('click', function() {
-    // Send a request to the server to simulate pressing '1'
-    fetch('/control?button=1', { method: 'POST' });
-    // You can handle the response if needed
-});
-// Add event listeners for other keypad buttons
