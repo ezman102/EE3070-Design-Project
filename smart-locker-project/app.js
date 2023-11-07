7dfd927a56082165f5fc0b4448010d244267af91
SIN Yuk Yuen <yysin8-c@my.cityu.edu.hk>
2023-11-07 13:43:43 +0800

    Update

--- smart-locker-project/app.js
+++ smart-locker-project/app.js
@@ -10,15 +10,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/home', (req, res) => {
-    // Ensure the 'title' property is being passed
-    res.render('home', { 
-        title: 'Home',
-        userType: 'YourUserType' // Make sure you replace 'YourUserType' with the actual user type.
-    });
+app.get('/', (req, res) => {
+  res.render('login');
 });
-
-
 
 const users = {
   student: '11111111',
@@ -27,20 +21,15 @@ const users = {
 };
 
 app.post('/login', (req, res) => {
-    const userType = req.body.username;  // Assuming 'username' is one of 'student', 'professor', 'security_guard'
-    const password = req.body.password;
-  
-    if (users[userType] && users[userType] === password) {
-      // Include the title in the render method
-      res.render('home', { 
-        title: 'Home',  // Add a suitable title
-        userType: userType 
-      });
-    } else {
-      res.send('Invalid username or password');
-    }
-  });
-  
+  const userType = req.body.username;  // Assuming 'username' is one of 'student', 'professor', 'security_guard'
+  const password = req.body.password;
+
+  if (users[userType] && users[userType] === password) {
+    res.render('home', { userType: userType });
+  } else {
+    res.send('Invalid username or password');
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
